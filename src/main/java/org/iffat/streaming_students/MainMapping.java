c9beab194d35f48046113192da3ffec439f44416
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-02 13:26:38 +0700

    18.Streams to Maps

---
+++ src/main/java/org/iffat/streaming_students/MainMapping.java
@@ -0,0 +1,58 @@
+package org.iffat.streaming_students;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.*;
+
+public class MainMapping {
+    public static void main(String[] args) {
+
+        Course pymc = new Course("PYMC", "Python Masterclass", 50);
+        Course jmc = new Course("JMC", "Java Masterclass", 100);
+        Course jgames = new Course("JGAME", "Creating games in Java");
+
+
+        List<Student> students = IntStream
+                .rangeClosed(1, 5000)
+                .mapToObj(s -> Student.getRandomStudent(jmc, pymc))
+                .toList();
+
+        var mappedStudents = students.stream()
+                .collect(Collectors.groupingBy(Student::getCountryCode));
+
+        mappedStudents.forEach((key, value) -> System.out.println(key + " " + value.size()));
+
+        System.out.println("-".repeat(90));
+        int minAge = 25;
+        var youngSet = students.stream()
+                .collect(groupingBy(Student::getCountryCode,
+                        filtering(s -> s.getAge() <= minAge, toList())));
+        youngSet.forEach((key, value) -> System.out.println(key + " " + value.size()));
+
+        var experienced = students.stream()
+                .collect(partitioningBy(Student::isProgrammingExperience));
+        System.out.println("Experienced Students = " + experienced.get(true).size());
+
+        var expCount = students.stream()
+                .collect(partitioningBy(Student::isProgrammingExperience, counting()));
+        System.out.println("Experienced Students = " + expCount.get(true));
+
+        var experiencedAndActive = students.stream()
+                .collect(partitioningBy(s -> s.isProgrammingExperience() && s.getMonthsSinceActive() == 0,
+                        counting()));
+        System.out.println("Experienced Students = " + experiencedAndActive.get(true));
+
+        var multiLevel = students.stream()
+                .collect(groupingBy(Student::getCountryCode,
+                        groupingBy(Student::getGender)));
+
+        multiLevel.forEach((key, value) -> {
+            System.out.println(key);
+            value.forEach((key1, value1) -> {
+                System.out.println("\t" + key1 + " " + value1.size());
+            });
+        });
+    }
+}
