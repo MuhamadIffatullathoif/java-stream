8fb93722d38bf5ee73fcfa5549b5d8642ff59b79
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-03 15:31:58 +0700

    28. Instant, ZonedDateTime, Duration, Period and ChronoUnit.between

--- src/main/java/org/iffat/more_time/Main.java
+++ src/main/java/org/iffat/more_time/Main.java
@@ -1,9 +1,12 @@
 package org.iffat.more_time;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
 
@@ -35,5 +38,72 @@ public class Main {
 
         Instant instantNow = Instant.now();
         System.out.println(instantNow);
+
+        for (ZoneId z : List.of(
+                ZoneId.of("Asia/Jakarta"),
+                ZoneId.of("Europe/Paris"),
+                ZoneId.of("America/New_York")
+        )) {
+            DateTimeFormatter zoneFormat = DateTimeFormatter.ofPattern("z:zzzz");
+            System.out.println(z);
+            System.out.println("\t" + instantNow.atZone(z).format(zoneFormat));
+            System.out.println("\t" + z.getRules().getDaylightSavings(instantNow));
+            System.out.println("\t" + z.getRules().isDaylightSavings(instantNow));
+        }
+
+        Instant dobInstant = Instant.parse("2020-01-01T08:01:00Z");
+        LocalDateTime dob = LocalDateTime.ofInstant(dobInstant, ZoneId.systemDefault());
+        System.out.println("Your kid's birthdate, LA time = " + dob.format(DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        ZonedDateTime dobJakarta = ZonedDateTime.ofInstant(dobInstant, ZoneId.of("Asia/Jakarta"));
+        System.out.println("Your kid's birthdate, Jakarta time = " + dobJakarta.format(DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        ZonedDateTime dobHere = dobJakarta.withZoneSameInstant(ZoneId.systemDefault());
+        System.out.println("Your kid's birthdate, time here = " + dobHere.format(DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        ZonedDateTime firstMonth = ZonedDateTime.now()
+                .with(TemporalAdjusters.firstDayOfNextMonth());
+        System.out.printf("First of next Month = %tD %n", firstMonth);
+
+        Period timePast = Period.between(LocalDate.EPOCH, dob.toLocalDate());
+        System.out.println(timePast);
+
+        Duration timeSince = Duration.between(Instant.EPOCH, dob.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince);
+
+        LocalDateTime dob2 = dob.plusYears(2).plusMonths(4)
+                .plusDays(4).plusHours(7).plusMinutes(14)
+                .plusSeconds(37);
+
+        System.out.println("Your 2nd kid's birthdate, time here = " + dob2.format(DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        Period timePast2 = Period.between(LocalDate.EPOCH, dob2.toLocalDate());
+        System.out.println(timePast2);
+
+        Duration timeSince2 = Duration.between(Instant.EPOCH, dob2.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince2);
+
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(LocalDate.EPOCH)) {
+                long val = u.between(LocalDate.EPOCH, dob2.toLocalDate());
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
+
+        LocalDateTime ldt = LocalDateTime.ofInstant(Instant.EPOCH, ZoneOffset.UTC);
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(ldt)) {
+                long val = u.between(ldt, dob2);
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
     }
 }
