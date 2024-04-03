d363e9c10d4a763ca4b19b34f9a3f3645154e004
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-03 22:49:25 +0700

    33. Internationalization (ResourceBundle) Part 2

--- src/main/java/org/iffat/resource_bundle_project/Main.java
+++ src/main/java/org/iffat/resource_bundle_project/Main.java
@@ -1,16 +1,32 @@
 package org.iffat.resource_bundle_project;
 
+import javax.swing.*;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ResourceBundle rb = ResourceBundle.getBundle("BasicText");
-        System.out.println(rb.getClass().getName());
-        System.out.println(rb.getBaseBundleName());
-        System.out.println(rb.keySet());
+        for (Locale locale : List.of(Locale.US, Locale.CANADA_FRENCH, Locale.CANADA)) {
+            ResourceBundle rb = ResourceBundle.getBundle("BasicText", locale);
+//            System.out.println(rb.getClass().getName());
+//            System.out.println(rb.getBaseBundleName());
+//            System.out.println(rb.keySet());
 
-        System.out.printf("%s %s!%n", rb.getString("hello"), rb.getString("world"));
+            String message = "%s %s!%n".formatted(rb.getString("hello"), rb.getString("world"));
+
+            ResourceBundle ui = ResourceBundle.getBundle("UIComponents", locale);
+
+            JOptionPane.showOptionDialog(null,
+                    message,
+                    ui.getString("first.title"),
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    new Object[]{rb.getString("yes"), rb.getString("no")},
+                    null);
+        }
     }
 }
